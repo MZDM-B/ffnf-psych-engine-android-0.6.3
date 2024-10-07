1bd56f357f76a5d42ec818d17bf8090f49dce2a4
MZDM-B <146510476+MZDM-B@users.noreply.github.com>
2024-10-07 11:49:04 +0800

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -88,6 +88,7 @@ class FPS extends TextField
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
+			text += "\nThis text just a demo awa";
 			#end
 
 			textColor = 0xFFFFFFFF;
